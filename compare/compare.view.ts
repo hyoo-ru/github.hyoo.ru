2134d4d1aaffdeb20d346da39bee59180fded20f
jin <jin@hyoo.ru>
2021-02-17 17:58:57 +0800

    Better interface, aggressive caching, more info.

--- compare/compare.view.ts
+++ compare/compare.view.ts
@@ -10,8 +10,7 @@ namespace $.$$ {
 		}
 
 		projects() {
-			const ids = this.project_ids()
-			return [ ... ids.map( ( id , index )=> this.Project( index ) ) , this.Project( ids.length ) ]
+			return this.project_ids().map( id => this.Project( id ) )
 		}
 
 		project_id( index : number , next? : string ) {
@@ -23,97 +22,157 @@ namespace $.$$ {
 			}
 			return ids[ index ] || ''
 		}
-
-		@ $mol_mem
-		capacities() {
-			return this.projects().map( project => project.capacity() ).filter(Boolean)
-		}
-
-		@ $mol_mem
-		project_labels() {
-			return this.projects().map( project => project.id() ).filter(Boolean)
-		}
-
-	}
-	
-	export class $hyoo_github_compare_project extends $.$hyoo_github_compare_project {
-
-		uri_project() {
-			return 'https://api.github.com/repos/' + this.id()
+		
+		add( next: string ) {
+			
+			if( next ) this.project_ids([
+				... new Set([
+					... this.project_ids(), next
+				])
+			])
+			
+			return ''
 		}
 		
-		uri_issues() {
-			return this.uri_project() + '/issues?per_page=100'
+		remove( id: string ) {
+			const ids = new Set( this.project_ids() )
+			ids.delete( id )
+			this.project_ids([ ... ids ])
+		}
+
+		id( id: string ) {
+			return id
+		}
+
+		uri_project( id: string ) {
+			return 'https://api.github.com/repos/' + id
 		}
 		
-		@ $mol_mem
-		project() {
-			return this.$.$mol_fetch.json( this.uri_project() ) as {
-				open_issues_count : number
-				homepage : string
-			}
+		uri_issues( id: string ) {
+			return this.uri_project( id ) + '/issues?per_page=100'
 		}
-
-		homepage() {
-			if( !this.id() ) return 'about:blank'
-			return this.project().homepage || 'https://github.com/' + this.id()
-		}
-
+		
 		@ $mol_mem_key
-		issues_page( page : number ) {
-			return this.$.$mol_fetch.json( this.uri_issues() + '&page=' + page ) as {
+		project( id: string ) {
+			
+			return this.$.$mol_shared.daily( id, ()=> {
+				
+				const res = this.$.$mol_fetch.json( this.uri_project( id ) ) as any
+				
+				return {
+					open_issues_count: res.open_issues_count as number,
+					homepage: res.homepage as string,
+				}
+				
+			} )
+			
+		}
+
+		homepage( id: string ) {
+			return this.project( id ).homepage || this.repo( id )
+		}
+
+		repo( id: string ) {
+			return 'https://github.com/' + id
+		}
+		
+		issues_link( id: string ) {
+			return this.repo( id ) + '/issues'
+		}
+		
+		issues_count( id: string ) {
+			return this.project( id ).open_issues_count
+		}
+
+		@ $mol_mem
+		issues_max() {
+			return this.project_ids().reduce( ( max, id )=> {
+				try {
+					return Math.max( max, this.issues_count( id ) )
+				} catch( error ) {
+					return max
+				}
+			} , 0 )
+		}
+		
+		issues_portion( id: string ) {
+			return this.issues_count( id ) / this.issues_max()
+		}
+		
+		@ $mol_mem_key
+		issues_page( { id, page } : { id: string, page: number } ) {
+			return this.$.$mol_fetch.json( this.uri_issues( id ) + '&page=' + page ) as {
 				created_at : string
 			}[]
 		}
 
 		@ $mol_mem_key
-		issues() {
+		issues( id: string ) {
 			return this.$.$mol_range2(
-				index => this.issues_page( Math.floor( index / 100 ) )[ index % 100 ] ,
-				()=> this.project().open_issues_count
+				index => this.issues_page({ id, page: Math.floor( index / 100 ) })[ index % 100 ] ,
+				()=> this.project( id ).open_issues_count
 			)
 		}
 
-		@ $mol_mem
-		capacity() {
-			if( !this.id() ) return 0
-
-			const key = 'project/' + this.id()
+		@ $mol_mem_key
+		capacity( id: string ) {
 			
-			try {
-
-				const cache = this.$.$mol_shared.cache<{ date : string , capacity : number }>( key ) ?? {}
-
-				if( cache.date ) {
-					const age = new this.$.$mol_time_interval( cache.date + '/' ).duration.count( 'P1D' )
-					if( age < 1 ) return cache.capacity
-				}
-
-			} catch( error ) {
-				if( error instanceof Promise ) return $mol_fail_hidden( error )
-				console.error( error )
-			}
-
-			const capacity = this.issues().reduce( ( sum , issue )=> {
-				const age = new this.$.$mol_time_interval( issue.created_at + '/' )
-				return sum + Math.ceil( age.duration.count( 'P1D' ) )
-			} , 0 )
-
-			const today  = new this.$.$mol_time_moment().merge({ hour : undefined , minute : undefined , second : undefined , offset : undefined })
-
-			this.$.$mol_shared.cache( key , {
-				date : today.toString() ,
-				capacity ,
+			return this.$.$mol_shared.daily( id + '/capacity', ()=> {
+				
+				const now = new this.$.$mol_time_moment
+				
+				return this.issues( id ).reduce( ( sum , issue )=> {
+					
+					const age = new this.$.$mol_time_interval({
+						start: issue.created_at,
+						end: now,
+					})
+					
+					return sum + Math.ceil( age.duration.count( 'P1D' ) )
+					
+				} , 0 )
+				
 			} )
 
-			return capacity
-		}
-
-		capacity_text() {
-			if( !this.id() ) return ''
-			return this.capacity().toLocaleString() + ' days'
 		}
 		
+		capacity_text( id: string ) {
+			return this.capacity( id ).toLocaleString() + ' days'
+		}
+		
+		@ $mol_mem
+		capacity_max() {
+			return this.project_ids().reduce( ( max, id )=> {
+				try {
+					return Math.max( max, this.capacity( id ) )
+				} catch( error ) {
+					return max
+				}
+			} , 0 )
+		}
+		
+		capacity_portion( id: string ) {
+			return this.capacity( id ) / this.capacity_max()
+		}
+		
+		@ $mol_mem
+		add_suggest() {
+			
+			const query = this.Add().filter_pattern()
+			if( !query ) return []
+			
+			this.$.$mol_wait_timeout( 1000 )
+			
+			return this.$.$mol_shared.daily( 'repo=' + query, ()=> {
+				
+				const uri = `https://api.github.com/search/repositories?q=${ encodeURIComponent( query ) }`
+				const res = this.$.$mol_fetch.json( uri ) as { items: { full_name: string }[] }
+				
+				return res.items.map( repo => repo.full_name )
+			} )
+			
+		}
+
 	}
 	
 }

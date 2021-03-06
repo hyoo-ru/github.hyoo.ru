7b7ffb6c9d7fa775a250f1ff38b810d7a990384b
nin-jin <nin-jin@users.noreply.github.com>
2021-03-06 07:17:14 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- node.js
+++ node.js
@@ -5625,7 +5625,7 @@ var $;
                 if (!event)
                     return event;
                 const keys = this.keys_y();
-                if (keys.length < 2)
+                if (keys.length < 1)
                     return;
                 const index_y = this.index_y();
                 const index_old = index_y === null ? 0 : index_y;
@@ -5639,7 +5639,7 @@ var $;
                 if (!event)
                     return event;
                 const keys = this.keys_y();
-                if (keys.length < 2)
+                if (keys.length < 1)
                     return;
                 const index_y = this.index_y();
                 const index_old = index_y === null ? keys.length - 1 : index_y;
@@ -5653,7 +5653,7 @@ var $;
                 if (!event)
                     return event;
                 const keys = this.keys_x();
-                if (keys.length < 2)
+                if (keys.length < 1)
                     return;
                 const index_x = this.index_x();
                 const index_old = index_x === null ? 0 : index_x;
@@ -5667,7 +5667,7 @@ var $;
                 if (!event)
                     return event;
                 const keys = this.keys_x();
-                if (keys.length < 2)
+                if (keys.length < 1)
                     return;
                 const index_x = this.index_x();
                 const index_old = index_x === null ? keys.length - 1 : index_x;
@@ -6163,10 +6163,7 @@ var $;
                 return value == null ? id : value;
             }
             option_rows() {
-                if (this.options_filtered().length === 0)
-                    return [this.No_options()];
-                let options = this.options_filtered().map((option) => this.Option_row(option));
-                return options;
+                return this.options_filtered().map((option) => this.Option_row(option));
             }
             option_focused(component) {
                 if (component == null) {
@@ -6186,7 +6183,12 @@ var $;
                 this.focused(false);
             }
             nav_components() {
-                return [this.Filter(), ...this.option_rows()];
+                if (this.options().length > 1 && this.Filter()) {
+                    return [this.Filter(), ...this.option_rows()];
+                }
+                else {
+                    return this.option_rows();
+                }
             }
             trigger_content() {
                 return [
@@ -6195,9 +6197,10 @@ var $;
                 ];
             }
             menu_content() {
-                return (this.options().length > 1 && this.Filter())
-                    ? [this.Filter(), ...this.option_rows()]
-                    : this.option_rows();
+                return [
+                    ...this.nav_components(),
+                    ...(this.options_filtered().length === 0) ? [this.No_options()] : []
+                ];
             }
         }
         __decorate([
--- node.test.js
+++ node.test.js
@@ -5618,7 +5618,7 @@ var $;
                 if (!event)
                     return event;
                 const keys = this.keys_y();
-                if (keys.length < 2)
+                if (keys.length < 1)
                     return;
                 const index_y = this.index_y();
                 const index_old = index_y === null ? 0 : index_y;
@@ -5632,7 +5632,7 @@ var $;
                 if (!event)
                     return event;
                 const keys = this.keys_y();
-                if (keys.length < 2)
+                if (keys.length < 1)
                     return;
                 const index_y = this.index_y();
                 const index_old = index_y === null ? keys.length - 1 : index_y;
@@ -5646,7 +5646,7 @@ var $;
                 if (!event)
                     return event;
                 const keys = this.keys_x();
-                if (keys.length < 2)
+                if (keys.length < 1)
                     return;
                 const index_x = this.index_x();
                 const index_old = index_x === null ? 0 : index_x;
@@ -5660,7 +5660,7 @@ var $;
                 if (!event)
                     return event;
                 const keys = this.keys_x();
-                if (keys.length < 2)
+                if (keys.length < 1)
                     return;
                 const index_x = this.index_x();
                 const index_old = index_x === null ? keys.length - 1 : index_x;
@@ -6156,10 +6156,7 @@ var $;
                 return value == null ? id : value;
             }
             option_rows() {
-                if (this.options_filtered().length === 0)
-                    return [this.No_options()];
-                let options = this.options_filtered().map((option) => this.Option_row(option));
-                return options;
+                return this.options_filtered().map((option) => this.Option_row(option));
             }
             option_focused(component) {
                 if (component == null) {
@@ -6179,7 +6176,12 @@ var $;
                 this.focused(false);
             }
             nav_components() {
-                return [this.Filter(), ...this.option_rows()];
+                if (this.options().length > 1 && this.Filter()) {
+                    return [this.Filter(), ...this.option_rows()];
+                }
+                else {
+                    return this.option_rows();
+                }
             }
             trigger_content() {
                 return [
@@ -6188,9 +6190,10 @@ var $;
                 ];
             }
             menu_content() {
-                return (this.options().length > 1 && this.Filter())
-                    ? [this.Filter(), ...this.option_rows()]
-                    : this.option_rows();
+                return [
+                    ...this.nav_components(),
+                    ...(this.options_filtered().length === 0) ? [this.No_options()] : []
+                ];
             }
         }
         __decorate([
